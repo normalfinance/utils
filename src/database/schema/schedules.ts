1af5b72a0611a3ba922d06c55dc31379073607b8
Joshua Blew <joshua@normalfinance.io>
2024-05-29 15:34:40 -0500

    fix: schedule schema fixes

--- src/database/schema/schedules.ts
+++ src/database/schema/schedules.ts
@@ -8,10 +8,10 @@ import {
   interval,
   integer,
   pgEnum,
-  decimal,
   boolean,
 } from 'drizzle-orm/pg-core';
 
+import { exchanges } from './exchanges';
 import { indexes } from './indexes';
 
 export type Schedule = typeof schedules.$inferSelect;
@@ -26,17 +26,21 @@ export const schedules = pgTable('schedules', {
   id: serial('id').primaryKey(),
   userId: uuid('userId').notNull(),
   exchangeId: integer('exchangeId').notNull(),
-  frequency: interval('frequency', {}).notNull(),
+  frequency: interval('frequency').notNull(),
   asset: varchar('asset', { length: 10 }),
   indexId: integer('indexId'),
   amountType: ScheduleAmountType('amountType').notNull(),
-  amount: decimal('amount').notNull(),
+  amount: varchar('amount').notNull(),
   active: boolean('active').default(true).notNull(),
   createdAt: timestamp('createdAt').defaultNow().notNull(),
   updatedAt: timestamp('updatedAt').defaultNow().notNull(),
 });
 
 export const schedulesRelations = relations(schedules, ({ one }) => ({
+  exchange: one(exchanges, {
+    fields: [schedules.exchangeId],
+    references: [exchanges.id],
+  }),
   index: one(indexes, {
     fields: [schedules.indexId],
     references: [indexes.id],
