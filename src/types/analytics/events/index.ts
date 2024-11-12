fdfbbe2d32cbfc8be193b75fd0b6308d4e2c587a
Joshua Blew <joshua@normalfinance.io>
2024-11-12 12:11:29 -0500

    refactor: remove deprecated lending code

--- src/types/analytics/events/index.ts
+++ src/types/analytics/events/index.ts
@@ -4,7 +4,6 @@ import type { DivestmentEventName } from './divestment';
 import type { ExchangeEventName } from './exchange';
 import type { IndexEventName } from './indexes';
 import type { InvestmentEventName } from './investment';
-import type { LendingEventName } from './lending';
 import type { NotificationEventName } from './notification';
 import type { ReferralEventName } from './referral';
 import type { ScheduleEventName } from './schedule';
@@ -15,7 +14,6 @@ export * from './divestment';
 export * from './exchange';
 export * from './indexes';
 export * from './investment';
-export * from './lending';
 export * from './notification';
 export * from './referral';
 export * from './schedule';
@@ -25,7 +23,6 @@ export type AnalyticsEventName =
   | BillingEventName
   | IndexEventName
   | ExchangeEventName
-  | LendingEventName
   | NotificationEventName
   | ReferralEventName
   | InvestmentEventName
